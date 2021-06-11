74c8fc52c7d6c39ac95050e7998366c56252a291
nikonru <makaroff.nickita2013@yandex.ru>
2021-06-11 14:28:33 +0300

    Check

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,12 @@
 
 int main() {
 	int number;
+	char c;
 	std::cout << "Hello world";
 	std::cout << "Enter Number:";
 	std::cin >> number;
+	std::cout << "Enter Character:";
+	std::cin >> c;
+	std::cout << c;
 	return 0;
 }
